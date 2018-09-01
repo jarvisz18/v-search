8ddf5abe6b7365819039ddd256fadf888aac5633
xxx_zha <ixan516@gmail.com>
2018-09-01 19:51:01 +0800

    Initial commit

---
+++ src/main/java/cn/ixan/search/SearchApplication.java
@@ -0,0 +1,11 @@
+package cn.ixan.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SearchApplication.class, args);
+	}
+}
