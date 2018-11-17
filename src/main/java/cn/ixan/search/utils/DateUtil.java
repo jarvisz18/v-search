5aedaa4f61be1f09091d970514254e9ec1e25a48
ixan516@gmail.com <huang3306>
2018-11-17 12:41:23 +0800

    日期工具类修改

--- src/main/java/cn/ixan/search/utils/DateUtil.java
+++ src/main/java/cn/ixan/search/utils/DateUtil.java
@@ -4,40 +4,14 @@ import org.apache.commons.lang3.time.DateUtils;
 
 import java.text.ParsePosition;
 import java.text.SimpleDateFormat;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Date;
 
 /**
- * 时间日期工具类,切勿重复造轮子
+ * 时间日期工具类,切勿重复造轮子(也可使用Java8日期API)
  * @see DateUtils
  * @author stack_zhang@outlook.com
  */
 public class DateUtil extends DateUtils{
-
-	/**
-	 * 返回某段时间的所有日期
-	 * @param dateBegin 开始时间
-	 * @param dateEnd 结束时间
-	 * @return 一段日期字符串
-	 */
-	public static List<String> findDates(Date dateBegin, Date dateEnd) {
-		List<String> lDate = new ArrayList<>();
-		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		lDate.add(format.format(dateBegin));
-		Calendar calBegin = Calendar.getInstance();
-		// 使用给定的 Date 设置此 Calendar 的时间
-		calBegin.setTime(dateBegin);
-		Calendar calEnd = Calendar.getInstance();
-		// 使用给定的 Date 设置此 Calendar 的时间
-		calEnd.setTime(dateEnd);
-		// 测试此日期是否在指定日期之后
-		while (dateEnd.after(calBegin.getTime())) {
-			// 根据日历的规则，为给定的日历字段添加或减去指定的时间量
-			calBegin.add(Calendar.DAY_OF_MONTH, 1);
-			lDate.add(format.format(calBegin.getTime()));
-		}
-		return lDate;
-	}
 
 	public static Date string2Date(String dateString){
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
@@ -45,23 +19,8 @@ public class DateUtil extends DateUtils{
 		return format.parse(dateString, position);
 	}
 
-	public static int differentTimeByMills(Date dateBegin, Date dateEnd){
-		int times = (int)((dateEnd.getTime()-dateBegin.getTime())/(1000*3600*24));
-		return times;
-	}
-	public static void main(String[] args) {
-		getList();
-	}
-
-	public static void getList(){
-		List<String> list = new ArrayList<>();
-		list.add("aaa");
-		list.add("abb");
-		list.add("sss");
-		list.add("ccc");
-		list.add("bba");
-		List<String> a = list.stream().filter(s -> s.contains("a")).collect(Collectors.toList());
-		a.forEach(System.out::println);
+	public static long differentTimeByMills(Date dateBegin, Date dateEnd){
+		return (dateEnd.getTime()-dateBegin.getTime())/(1000*3600*24);
 	}
 
 }
