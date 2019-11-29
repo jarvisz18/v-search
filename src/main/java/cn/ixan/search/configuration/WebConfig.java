bc27dea11b6d830f93355ce5f1bdba9a24bde2ed
张贤龙 <stack_zhang@outlook.com>
2019-11-29 22:33:08 +0800

    设置静态资源缓存

--- src/main/java/cn/ixan/search/configuration/WebConfig.java
+++ src/main/java/cn/ixan/search/configuration/WebConfig.java
@@ -1,25 +1,25 @@
 package cn.ixan.search.configuration;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import java.util.concurrent.TimeUnit;
 
 
-//@Configuration
+@Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-	//todo:添加静态资源缓存注册
+	/**
+	 * <p>设置静态资源缓存时间:1年</p>
+	 * <p>注意:不可缓存入口文件</p>
+	 * @param registry 资源注册
+	 */
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry
-				.addResourceHandler("/resources/**")
-				.addResourceLocations("/templates/")
-				.setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS)
-						.cachePrivate()
-						.mustRevalidate())
-				.resourceChain(true)
-				.addResolver(new PathResourceResolver());
+		registry.addResourceHandler("/*.js","/*.css")
+				.addResourceLocations("classpath:/static/")
+				.setCacheControl(CacheControl.maxAge(365,TimeUnit.DAYS).cachePublic())
+				.setCachePeriod(3153600);
 	}
 }
