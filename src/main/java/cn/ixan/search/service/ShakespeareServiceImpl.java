e4f62345ca586b93dc790dc51f1097d693848aee
张贤龙 <stack_zhang@outlook.com>
2019-12-08 17:25:30 +0800

    1.修改注解注入方式

--- src/main/java/cn/ixan/search/service/ShakespeareServiceImpl.java
+++ src/main/java/cn/ixan/search/service/ShakespeareServiceImpl.java
@@ -20,10 +20,10 @@ import org.assertj.core.util.Lists;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 
+import javax.annotation.Resource;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,13 +35,13 @@ import java.util.stream.Collectors;
 @Service
 public class ShakespeareServiceImpl implements ShakespeareService{
     private static final Logger log = LoggerFactory.getLogger(ShakespeareServiceImpl.class);
-    @Autowired
+    @Resource
     private Gson gson;
-    @Autowired
+    @Resource
     private JestClient jestClient;
-    @Autowired
+    @Resource
     private ShakespeareMapper shakespeareMapper;
-    @Autowired
+    @Resource
     private SqlSessionTemplate sqlSessionTemplate;
 
     public boolean existIndex(String indexName, String indexType) {
