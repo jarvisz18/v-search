ffcdfa94781f0df25e6432331e46f6a26e7b7098
ixan <stack_zhang@outlook.com>
2019-12-22 10:44:29 +0800

    修改了测试类

--- src/main/java/cn/ixan/search/controller/TestController.java
+++ src/main/java/cn/ixan/search/controller/TestController.java
@@ -1,23 +1,23 @@
 package cn.ixan.search.controller;
 
+import cn.ixan.search.utils.DateUtil;
 import cn.ixan.search.utils.UUIDUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author stack_zhang@outlook.com
  */
-@Controller
+@RestController
 @Slf4j
 public class TestController {
+
 	@GetMapping("/test")
-	public String testCheckTime(@ModelAttribute String start,
-								@ModelAttribute String end){
+	public String testCheckTime(){
 		String uuid = UUIDUtils.uuid();
-		log.info("开始时间[{}]",start);
-		log.info("结束时间[{}]",end);
+		log.info("开始时间[{}]", DateUtil.currentTime());
+		log.info("结束时间[{}]",DateUtil.currentTime());
 		log.info("获取到数据:[{}]",uuid);
 		return uuid;
 
