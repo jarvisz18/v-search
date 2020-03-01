5af8fd1fe07ee0ea4479cd8c4166fd2d9758742b
张贤龙 <stack_zhang@outlook.com>
2020-03-01 18:07:42 +0800

    增加valid校验测试类

---
+++ src/main/java/cn/ixan/search/domain/valid/Update.java
@@ -0,0 +1,10 @@
+package cn.ixan.search.domain.valid;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @version 1.0
+ * @date Created in 2020/3/1 14:05
+ * @description second
+ */
+public interface Update {
+}
