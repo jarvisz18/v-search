98bf2582ac349c803997dd7941da6ac65320ffb1
ixan <stack_zhang@outlook.com>
2020-07-01 22:40:50 +0800

    1.增加valid测试类

---
+++ boot-search/src/main/java/cn/ixan/search/web/controller/HelloController.java
@@ -0,0 +1,18 @@
+package cn.ixan.search.web.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Random;
+
+@RestController
+@Slf4j
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String test1(){
+        log.info("hello----------");
+        return "hello world!"+new Random().nextInt(100);
+    }
+}
