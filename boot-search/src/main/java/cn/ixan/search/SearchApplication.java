f97a4d55503f014a1edf508d049a4763892c51ad
张贤龙 <stack_zhang@outlook.com>
2020-09-06 11:58:36 +0800

    1.整合spring-kafka

--- boot-search/src/main/java/cn/ixan/search/SearchApplication.java
+++ boot-search/src/main/java/cn/ixan/search/SearchApplication.java
@@ -1,7 +1,9 @@
 package cn.ixan.search;
 
+import cn.ixan.search.web.controller.Product;
 import com.google.gson.Gson;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.CacheManager;
@@ -13,6 +15,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+import javax.annotation.PostConstruct;
 import java.util.Collections;
 
 /**
@@ -29,8 +32,18 @@ public class SearchApplication {
 		SpringApplication.run(SearchApplication.class, args);
 	}
 
+	@Autowired
+	private Product product;
+
+	@PostConstruct
+	public void init() {
+		for (int i = 0; i < 10; i++) {
+			product.send("v-search" + i);
+		}
+	}
+
 	@Bean
-	public Gson gson(){
+	public Gson gson() {
 		return new Gson();
 	}
 
---
+++ boot-search/src/main/java/cn/ixan/search/web/controller/Product.java
@@ -0,0 +1,27 @@
+package cn.ixan.search.web.controller;
+
+import cn.ixan.search.web.controller.dto.UserDTO;
+import com.google.gson.Gson;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @version 1.0
+ * @date Created in 2020/9/4 13:42
+ * @description product message
+ */
+@Component
+public class Product {
+	@Resource
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void send(String name) {
+		UserDTO u = new UserDTO();
+		u.setUsername(name);
+		u.setAge(11);
+		kafkaTemplate.send("test", new Gson().toJson(u));
+	}
+}
