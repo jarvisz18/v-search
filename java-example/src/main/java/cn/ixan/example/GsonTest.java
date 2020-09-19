3ec6728d7aacfae2e7c0614b849fbec7316c288d
张贤龙 <stack_zhang@outlook.com>
2020-09-19 22:23:59 +0800

    1.新增GSON

---
+++ java-example/src/main/java/cn/ixan/example/GsonTest.java
@@ -0,0 +1,28 @@
+package cn.ixan.example;
+
+import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @date Created in 2020/9/19 22:17
+ * @description GsonTest
+ * @version 1.0
+ */
+public class GsonTest {
+	public static void main(String[] args) {
+		Gson gson = new Gson();
+		HashMap<Object, Object> map = Maps.newHashMap();
+		map.put("color", "red");
+		map.put("weight", "10kg");
+		//对象转JSON
+		String s = gson.toJson(map);
+		System.out.println(s);
+		//JSON转MAP
+		Map fromJson = gson.fromJson(s, Map.class);
+		System.out.println(fromJson);
+	}
+}
