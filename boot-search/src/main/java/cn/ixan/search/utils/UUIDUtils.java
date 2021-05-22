88e63a090e2eb14436170a6759cd86022c8bf5b1
zhangxianlong <stackzhang@outlook.com>
2021-05-22 12:11:05 +0800

    guid

--- boot-search/src/main/java/cn/ixan/search/utils/UUIDUtils.java
+++ boot-search/src/main/java/cn/ixan/search/utils/UUIDUtils.java
@@ -1,12 +1,24 @@
 package cn.ixan.search.utils;
 
+import cn.ixan.search.utils.base.TimeBasedUUIDGenerator;
+import cn.ixan.search.utils.base.UUIDGenerator;
+
 import java.util.UUID;
 
 public final class UUIDUtils {
+	public static void main(String[] args) {
+		System.out.println(guid());
+	}
 
     public static String uuid() {
         return UUID.randomUUID().toString().replace("-", "");
     }
+
+	public static String guid() {
+		UUIDGenerator uuidGenerator = new TimeBasedUUIDGenerator();
+		return uuidGenerator.getBase64UUID();
+
+	}
 
     public static String generate() {
         return UUID.randomUUID().toString();
