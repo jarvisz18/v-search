4c3b7d3577474bf68f8472299ba97d0aa935069d
张贤龙 <stack_zhang@outlook.com>
2021-06-22 10:35:56 +0800

    1.获取给定日期的差值月的第几个工作日

--- boot-test/src/main/java/com/ixan/boot/utils/DateTool.java
+++ boot-test/src/main/java/com/ixan/boot/utils/DateTool.java
@@ -1,7 +1,12 @@
 package com.ixan.boot.utils;
 
+import com.google.common.collect.Sets;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Set;
 
 /**
  * @author stack_zhang@outlook.com
@@ -10,6 +15,38 @@ import java.time.format.DateTimeFormatter;
  * @version 1.0
  */
 public class DateTool {
+	/**
+	 * 获取给定日期的差值月的第几个工作日(周一至周五)
+	 * @param localDate 指定日期
+	 * @param dValue 当前日期的差值月
+	 * @param dayOfWork 第几个工作日
+	 * @return LocalDate yyyy-MM-dd
+	 */
+	public static LocalDate getFirstInMonthDay(LocalDate localDate, int dValue, int dayOfWork) {
+		//工作日
+		Set<DayOfWeek> dayOfWorkSet = Sets.newHashSet(DayOfWeek.MONDAY, DayOfWeek.TUESDAY,
+				DayOfWeek.WEDNESDAY, DayOfWeek.THURSDAY, DayOfWeek.FRIDAY);
+
+		LocalDate tempDate = localDate.plusMonths(dValue);
+		//count 代表每月第几天,i 代表第几个工作日
+		for (int i = 0, count = 1; i < dayOfWork; ) {
+			tempDate = tempDate.withDayOfMonth(count);
+			DayOfWeek dayOfWeek = tempDate.getDayOfWeek();
+			if (dayOfWorkSet.contains(dayOfWeek)) {
+				i++;
+			}
+			count++;
+		}
+		return tempDate;
+	}
+
+	public static void main(String[] args) {
+		LocalDate now = LocalDate.now();
+		LocalDate firstInMonthDay = getFirstInMonthDay(now, -1, 3);
+		System.out.println(firstInMonthDay);
+	}
+
+
 	public static String getCurrentDateTime() {
 		LocalDateTime localDateTime = LocalDateTime.now();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
