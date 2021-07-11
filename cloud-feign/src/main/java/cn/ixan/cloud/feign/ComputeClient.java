748e495905f4cf5df0876a1e3117c4d037636787
zhangxianlong <stackzhang@outlook.com>
2021-07-11 21:23:27 +0800

    增加注解

--- cloud-feign/src/main/java/cn/ixan/cloud/feign/ComputeClient.java
+++ cloud-feign/src/main/java/cn/ixan/cloud/feign/ComputeClient.java
@@ -1,13 +1,15 @@
 package cn.ixan.cloud.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Primary
 @FeignClient(name = "compute-service",fallback = ComputeClientHystrix.class)
 public interface ComputeClient {
 
-    @RequestMapping(method = RequestMethod.GET,value = "/add")
-    Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
+	@RequestMapping(method = RequestMethod.GET,value = "/add")
+	Integer add(@RequestParam(value = "a") Integer a, @RequestParam(value = "b") Integer b);
 }
