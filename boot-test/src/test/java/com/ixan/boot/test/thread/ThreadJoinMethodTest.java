d9fd9d9fa6f53960d752cfe4322b3ebe2d0e0ca0
张贤龙 <stack_zhang@outlook.com>
2021-07-17 15:54:28 +0800

    1.join的用法

--- boot-test/src/test/java/com/ixan/boot/test/thread/ThreadJoinMethodTest.java
+++ boot-test/src/test/java/com/ixan/boot/test/thread/ThreadJoinMethodTest.java
@@ -7,6 +7,11 @@ package com.ixan.boot.test.thread;
  * @description 测试线程等待的join方法
  */
 public class ThreadJoinMethodTest {
+	/**
+	 * 经典题目:
+	 * 现有T1、T2、T3三个线程，你怎样保证T2在T1执行完后执行，
+	 * T3在T2执行完后执行
+	 */
 	public static void main(String[] args) throws InterruptedException {
 		Thread threadOne = new Thread(new Runnable() {
 			@Override
@@ -32,15 +37,29 @@ public class ThreadJoinMethodTest {
 			}
 		});
 
+		Thread threadThree = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					Thread.sleep(1000L);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				System.out.println("child ThreadThree over");
+			}
+		});
+
 		//启动子线程
 		threadOne.start();
 		threadTwo.start();
+		threadThree.start();
 
 		System.out.println("wait all child thread over");
 
 		//等待子线程执行完毕，返回
 		threadOne.join();
 		threadTwo.join();
+		threadThree.join();
 
 		System.out.println("all child thread over");
 
