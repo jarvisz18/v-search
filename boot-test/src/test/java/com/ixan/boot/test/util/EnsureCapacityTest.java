5a9c17856fdb16d3138c4cc6d848b75c0bc865e9
zhangxianlong <stackzhang@outlook.com>
2021-08-11 20:14:05 +0800

    1.新增测试类 2.新增Spring工具类

---
+++ boot-test/src/test/java/com/ixan/boot/test/util/EnsureCapacityTest.java
@@ -0,0 +1,22 @@
+package com.ixan.boot.test.util;
+
+import java.util.ArrayList;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @version 1.0
+ * @date Created in 2021/8/8 下午12:29
+ * @description EnsureCapacityTest
+ */
+public class EnsureCapacityTest {
+	public static void main(String[] args) {
+		ArrayList<Object> list = new ArrayList<Object>();
+		final int N = 10000000;
+		long startTime = System.currentTimeMillis();
+		for (int i = 0; i < N; i++) {
+			list.add(i);
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("使用ensureCapacity方法前：" + (endTime - startTime));
+	}
+}
