e3ca77b3fd41328db7ae0eb575ff044ce1c25d1c
zhangxianlong <stackzhang@outlook.com>
2021-09-16 21:47:29 +0800

    spring 事件驱动

---
+++ boot-test/src/main/java/com/ixan/boot/service/DriverListener.java
@@ -0,0 +1,39 @@
+package com.ixan.boot.service;
+
+import com.ixan.boot.domain.event.DriverChangeEvent;
+import com.ixan.boot.utils.ConnectPool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @version 1.0
+ * @date Created in 2021/9/16 下午9:13
+ * @description 监听器
+ */
+@Service
+public class DriverListener implements ApplicationListener<DriverChangeEvent> {
+	private static final Logger logger = LoggerFactory.getLogger(DriverListener.class);
+
+	@Override
+	public void onApplicationEvent(DriverChangeEvent driverChangeEvent) {
+		String id = driverChangeEvent.getId();
+		Integer op = driverChangeEvent.getOp();
+		logger.info("receive event change id:[{}],op:[{}]", id, op);
+		switch (op) {
+			case 0:
+			case 1:
+				ConnectPool.POOL_MAP.put(id, id);
+				break;
+			case 2:
+				ConnectPool.POOL_MAP.remove(id);
+				break;
+			default:
+				throw new RuntimeException("暂不支持此操作");
+		}
+		ConnectPool.POOL_MAP.forEach((k, v) -> logger.info("k:[{}],v:[{}]", k, v));
+		logger.info("--------------------------------------------------");
+	}
+}
---
+++ boot-test/src/main/java/com/ixan/boot/service/DriverService.java
@@ -0,0 +1,33 @@
+package com.ixan.boot.service;
+
+import com.ixan.boot.domain.event.DriverChangeEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author stack_zhang@outlook.com
+ * @version 1.0
+ * @date Created in 2021/9/16 下午9:06
+ * @description 驱动管理类
+ */
+@Service
+public class DriverService implements ApplicationEventPublisherAware {
+	private static final Logger logger = LoggerFactory.getLogger(DriverService.class);
+
+	private ApplicationEventPublisher applicationEventPublisher;
+
+	@Override
+	public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+		this.applicationEventPublisher = applicationEventPublisher;
+	}
+
+	public void change(String id, Integer op) {
+		//执行业务逻辑
+		logger.info("driver id:[{}] is change,op:[{}]", id, op);
+		//发布事件
+		applicationEventPublisher.publishEvent(new DriverChangeEvent(this, id, op));
+	}
+}
