620818b9410a153a2f842638dad89ba00bd3d020
zhangxianlong <stackzhang@outlook.com>
2021-10-23 20:57:38 +0800

    线程的三种创建方式

--- boot-test/src/test/java/com/ixan/boot/test/thread/ThreadTest.java
+++ boot-test/src/test/java/com/ixan/boot/test/thread/ThreadTest.java
@@ -1,5 +1,9 @@
 package com.ixan.boot.test.thread;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.FutureTask;
+
 /**
  * @author stack_zhang@outlook.com
  * @version 1.0
@@ -7,14 +11,55 @@ package com.ixan.boot.test.thread;
  * @description 继承Thread类的线程创建方式
  */
 public class ThreadTest {
+	//创建有返回值的task
+	public static class CallerTask implements Callable<String> {
+		public String call() throws Exception {
+			return "hello";
+		}
+	}
+
+	//继承Runnable并重写run方法
+	public static class RunableTask implements Runnable {
+		public void run() {
+			System.out.println("i am a child thread");
+		}
+	}
+
+	//继承Thread类并重写run方法
 	public static class MyThread extends Thread {
 		@Override
 		public void run() {
-			System.out.println("i am a child Thread");
+			System.out.println("i am a child thread");
+			System.out.println("i am a child thread");
+			System.out.println("i am a child thread");
 		}
 	}
 
 	public static void main(String[] args) {
+		testThreadTask();
+		System.out.println("---------------");
+		testRunnableTeak();
+
+		//创建异步任务
+		FutureTask<String> stringFutureTask = new FutureTask<>(new CallerTask());
+		new Thread(stringFutureTask).start();
+		String result = null;
+		try {
+			result = stringFutureTask.get();
+		} catch (InterruptedException | ExecutionException e) {
+			e.printStackTrace();
+		}
+		System.out.println(result);
+
+	}
+
+	private static void testRunnableTeak() {
+		RunableTask task = new RunableTask();
+		new Thread(task).start();
+		new Thread(task).start();
+	}
+
+	private static void testThreadTask() {
 		//创建线程
 		MyThread myThread = new MyThread();
 		//启动线程
