c31134fe9d8391369f6f1a82743cd50511cd524e
zhangxianlong <stackzhang@outlook.com>
2022-01-03 11:09:45 +0800

    生产者-消费者阻塞队列模式修改

--- boot-test/src/test/java/com/ixan/boot/test/juc/queue/BlockingQueueTaskTest.java
+++ boot-test/src/test/java/com/ixan/boot/test/juc/queue/BlockingQueueTaskTest.java
@@ -22,13 +22,20 @@ public class BlockingQueueTaskTest {
 		ArrayBlockingQueue<String> queue = new ArrayBlockingQueue<>(MAX_QUEUE_SIZE);
 		final int NCPUS = Runtime.getRuntime().availableProcessors();
 		LOGGER.info("Runtime.getRuntime().availableProcessors() :[{}]", NCPUS);
-		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(NCPUS, NCPUS, 2000L, TimeUnit.MICROSECONDS,
-				new ArrayBlockingQueue<>(100),
+		//线程池1
+		ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(1, 2, 2000L, TimeUnit.MICROSECONDS,
+				new ArrayBlockingQueue<>(1),
 				new ThreadFactoryBuilder().setNameFormat("doTask-%d").build(), new ThreadPoolExecutor.CallerRunsPolicy());
+
+		//线程池2
+		ThreadPoolExecutor consumerPoolExecutor = new ThreadPoolExecutor(NCPUS, NCPUS, 2000L, TimeUnit.MICROSECONDS,
+				new ArrayBlockingQueue<>(100),
+				new ThreadFactoryBuilder().setNameFormat("doConsumer-%d").build(), new ThreadPoolExecutor.CallerRunsPolicy());
 
 		List<Future<Integer>> futureList = new ArrayList<>();
 		try {
-			Future<Integer> submit = threadPoolExecutor.submit(new ConsumerTask(queue));
+			Future<Integer> submit = threadPoolExecutor.submit(new ConsumerTask(queue, consumerPoolExecutor));
+			TimeUnit.SECONDS.sleep(1L);
 			futureList.add(submit);
 			//当前线程是生产者
 			for (int i = 0; i <= 980; i++) {
@@ -44,23 +51,31 @@ public class BlockingQueueTaskTest {
 			e.printStackTrace();
 		} finally {
 			//关闭线程池
+			consumerPoolExecutor.shutdown();
 			threadPoolExecutor.shutdown();
 		}
 	}
 
 	private static class ConsumerTask implements Callable<Integer> {
 		private final BlockingQueue<String> blockingQueue;
+		private final ThreadPoolExecutor threadPoolExecutor;
 
-		ConsumerTask(BlockingQueue<String> blockingQueue) {
+		ConsumerTask(BlockingQueue<String> blockingQueue, ThreadPoolExecutor consumerPoolExecutor) {
 			this.blockingQueue = blockingQueue;
+			this.threadPoolExecutor = consumerPoolExecutor;
 		}
 
 		@Override
 		public Integer call() throws Exception {
 			int i = 0;
-			while (!blockingQueue.isEmpty()) {
-				String take = blockingQueue.take();
-				LOGGER.info("get element :{}", take);
+			while (true) {
+				String take = blockingQueue.poll(5L, TimeUnit.SECONDS);
+				if (null == take) {
+					break;
+				}
+				threadPoolExecutor.submit(() -> {
+					LOGGER.info("get element :{},do insert", take);
+				});
 				i++;
 			}
 			return i;
