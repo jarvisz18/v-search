0294ff5a96dda3e209c3b8674416abd320eb5d69
zhangxianlong <stackzhang@outlook.com>
2022-03-15 21:42:28 +0800

    volatile关键字

---
+++ java-juc/src/main/java/com/ixan/boot/test/juc/basic/VolatileTest.java
@@ -0,0 +1,10 @@
+package com.ixan.boot.test.juc.basic;
+
+/**
+ * @author stackzhang@126.com
+ * @version 1.0
+ * @date Created in 2022/3/15 下午9:41
+ * @description volatile关键字测试
+ */
+public class VolatileTest {
+}
