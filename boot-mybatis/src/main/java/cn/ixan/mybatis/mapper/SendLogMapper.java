b19eacf43b0bcc086727c1bb6aab943de731ca34
stackzhang <stackzhang@126.com>
2023-06-24 23:47:37 +0800

    add mybatis example

---
+++ boot-mybatis/src/main/java/cn/ixan/mybatis/mapper/SendLogMapper.java
@@ -0,0 +1,18 @@
+package cn.ixan.mybatis.mapper;
+
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.Map;
+
+/**
+ * @author stackzhang@126.com
+ * @date Created in 2023/6/24 22:10
+ * @description send log mapper
+ */
+public interface SendLogMapper {
+	Integer getLogCount();
+
+	@MapKey("id")
+	Map<String,Object> findById(@Param("id") Integer id);
+}
---
+++ boot-mybatis/src/test/java/cn/ixan/mybatis/mapper/SendLogMapperTest.java
@@ -0,0 +1,45 @@
+package cn.ixan.mybatis.mapper;
+
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class SendLogMapperTest {
+	private SqlSession sqlSession;
+	private SendLogMapper mapper;
+
+	@Before
+	public void before() throws Exception {
+		InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+		sqlSession = sqlSessionFactory.openSession();
+		mapper = sqlSession.getMapper(SendLogMapper.class);
+	}
+
+
+	@Test
+	public void getLogCount() {
+		Integer logCount = mapper.getLogCount();
+		System.out.println(logCount);
+	}
+
+	@Test
+	public void findById() {
+		Map<String, Object> resultMap = mapper.findById(1);
+		System.out.println(resultMap);
+	}
+
+
+	@After
+	public void after() {
+		sqlSession.close();
+	}
+}
